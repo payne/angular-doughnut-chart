8d9a5eb2497d530d6e243b39bb29f3f3111a1931
Matt Payne <Payne@MattPayne.org>
2019-08-30 19:48:23 -0500

    Added third dataset

--- src/app/doughnut/doughnut.ts
+++ src/app/doughnut/doughnut.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
- 
+
 @Component({
   selector: 'doughnut',
   templateUrl: './doughnut.html'
 })
 export class DoughnutChartComponent {
   // Doughnut
-  public doughnutChartLabels:string[] = ['Age 18 to 24', 'Age 25 to 35', 'Above 35+'];
-  public demodoughnutChartData:number[] = [[350, 450, 100],[250, 350, 150]];
-  public doughnutChartType:string = 'doughnut';
- 
+  public doughnutChartLabels: string[] = ['Age 18 to 24', 'Age 25 to 35', 'Above 35+'];
+  public demodoughnutChartData: number[] = [
+    [350, 450, 100],
+    [50, 150, 300],
+    [250, 350, 150]
+  ];
+  public doughnutChartType: string = 'doughnut';
+
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+
+  public chartHovered(e: any): void {
     console.log(e);
   }
 }
